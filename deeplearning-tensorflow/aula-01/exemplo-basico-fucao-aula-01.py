52d9afd05d21e1867234c2ef45e1700fcac52673
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-06-22 00:14:57 -0300

    criei

---
+++ deeplearning-tensorflow/aula-01/exemplo-basico-fucao-aula-01.py
@@ -0,0 +1,11 @@
+'''
+o exeplo é para descobrir uma função
+intradas: X= -1, 0, 0, 1, 2, 3, 4
+saidas: -3, -1, 1, 3, 5, 7
+função: y = 2 * x - 1
+
+float hw_function(float x){
+    float y = ( 2 * x ) - 1;
+    return y;
+}
+'''
