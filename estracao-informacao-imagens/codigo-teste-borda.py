4973e89cd1a52c751f9e92f714b3a4f427bf612a
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-07-21 16:49:07 -0300

    atualizando

--- estracao-informacao-imagens/codigo-teste-borda.py
+++ estracao-informacao-imagens/codigo-teste-borda.py
@@ -1,17 +1,39 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
-img = cv.imread('img/entrada/folha-de-mamao-menor.jpg')
-
+img = cv2.imread('img/entrada/sigatoka3.jpeg')
+imgOriginal = img;
 # Converter BGR em HSV
-hsv = cv.cvtColor (img, cv.COLOR_BGR2HSV)
+hsv = cv2.cvtColor (img, cv2.COLOR_BGR2HSV)
 # define a faixa de cor azul em HSV
 lower_blue = np.array ([0, 30, 30])
 upper_blue = np.array ([130,255,255])
 # Limite a imagem HSV para obter apenas cores azuis
-mask = cv.inRange (hsv, lower_blue, upper_blue)
+mask = cv2.inRange (hsv, lower_blue, upper_blue)
 # Bitwise-AND máscara e imagem original
-res = cv.bitwise_and (img, img, mask = mask)
+img = cv2.bitwise_and (img, img, mask = mask)
 
-cv.imshow('Bordas retira de azul',res)
-cv.waitKey(0)
+cv2.imshow('Bordas retira de azul',img)
+#---------------------------------------------------
+imgray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY) #deixa preto em branco
+suave = cv2.GaussianBlur(imgray, (7, 7), 0) #metodo para fazer a suavização
+bordas =  cv2.Canny(suave,100,200) #metodo de para retirar as borads
+
+ret, thresh = cv2.threshold (bordas, 127, 255, 0) #detodo bara indentificar as bordas
+contornos, hierarquia = cv2.findContours (thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #detodo bara indentificar as bordas
+
+#--------------------- Dezenhando a borda -------------------------------
+cv2.drawContours (img, contornos, -1, (255,0,255), 3)
+#cv2.drawContours (img, contornos, 235, (0,255,0), 3) #para celecionar uma borda em especifico basta subistituir o "-1"
+'''cnt = contornos[0]
+cv2.drawContours(img, [cnt], 0, (0,255,0), 3)''' #funcão para mostra um contorno em expecifico
+print("Numeros de contornos = " + str(len(contornos)))
+
+
+
+cv2.imshow('Imagem',img)
+cv2.imshow('Imagem gray',imgray)
+cv2.imshow('Bordas Canny',bordas)
+cv2.imshow('imgOriginal',imgOriginal)
+
+cv2.waitKey(0)
--- estracao-informacao-imagens/contorno-da-borda.py
+++ estracao-informacao-imagens/contorno-da-borda.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 
 img = cv2.imread ( 'img/entrada/sigatoka.jpg' )
+imagemOrinal = img;
+cv2.imshow('imagemOrinal',imagemOrinal)
 imgray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY) #deixa preto em branco
 suave = cv2.GaussianBlur(imgray, (7, 7), 0) #metodo para fazer a suavização
 bordas =  cv2.Canny(suave,100,200) #metodo de para retirar as borads
@@ -10,8 +12,8 @@ ret, thresh = cv2.threshold (bordas, 127, 255, 0) #detodo bara indentificar as b
 contornos, hierarquia = cv2.findContours (thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #detodo bara indentificar as bordas
 
 #--------------------- Dezenhando a borda -------------------------------
-cv2.drawContours (img, contornos, -1, (255,0,255), 3)
-#cv2.drawContours (img, contornos, 2, (0,255,0), 3) #para celecionar uma borda em especifico basta subistituir o "-1"
+#cv2.drawContours (img, contornos, -1, (255,0,255), 3)
+cv2.drawContours (img, contornos, 2, (0,255,0), 3) #para celecionar uma borda em especifico basta subistituir o "-1"
 '''cnt = contornos[0]
 cv2.drawContours(img, [cnt], 0, (0,255,0), 3)''' #funcão para mostra um contorno em expecifico
 print("Numeros de contornos = " + str(len(contornos)))
@@ -21,4 +23,5 @@ print("Numeros de contornos = " + str(len(contornos)))
 cv2.imshow('Imagem',img)
 cv2.imshow('Imagem gray',imgray)
 cv2.imshow('Bordas Canny',bordas)
+
 cv2.waitKey(0)
