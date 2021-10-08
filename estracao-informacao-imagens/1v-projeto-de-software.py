c7e39e4f66d49e2379216cec9ea630464a75a29f
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-10-08 15:30:06 -0300

    .

--- estracao-informacao-imagens/1v-projeto-de-software.py
+++ estracao-informacao-imagens/1v-projeto-de-software.py
@@ -3,7 +3,18 @@ from glob import glob
 import indentificacaoDaDoenca2 as IndDoen
 
 arquivos = sorted(glob('img/mamao/*.jpg'))
-print(str(arquivos[0]))
+for i in range(len(arquivos)):
+    print(str(arquivos[0]))
+    imagem = cv2.imread(arquivos[i])
+    cv2.imshow('Imagem', imagem)
+    cv2.waitKey(0)
 #img1 = IndDoen.Detecta_Doenca(img)
 #cv2.imshow("Doença RGB", img.ConvercaoYCrCb())
 #cv2.waitKey(0)
+
+imagem1 = cv2.imread("img/entrada/folha-de-mamao-sem-fundo.jpg")
+
+'''img1 = IndDoen.Detecta_Doenca(imagem1)
+img1.Convercao_Cores()
+img1.Segmentar()
+img1.Aplicando_Mascara()'''
